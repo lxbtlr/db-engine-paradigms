32bcff7a251fc2072795175daad912d1affcd966
Alex Butler <lxbtlr@pm.me>
2026-06-03 11:23:46 -0500

    add huge page info incase we want to try 1gb pages

--- include/common/runtime/Memory.hpp
+++ include/common/runtime/Memory.hpp
@@ -1,6 +1,13 @@
 #pragma once
 #include <stdexcept>
 #include <sys/mman.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define MAP_HUGE_2MB    (21 << MAP_HUGE_SHIFT)
+#define MAP_HUGE_1GB    (30 << MAP_HUGE_SHIFT)
+
+
 
 namespace mem {
 inline void* malloc_huge(size_t size) {
@@ -8,6 +15,8 @@ inline void* malloc_huge(size_t size) {
                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 #ifdef __linux__
    madvise(p, size, MADV_HUGEPAGE);
+   //madvise(p, size, MADV_NOHUGEPAGE);
+   // memset(p, 0, size);
 #endif
    // memset(p, 0, size);
    return p;
