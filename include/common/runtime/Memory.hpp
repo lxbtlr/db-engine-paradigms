be787a724700cdb803542a8a583088dc7b89a4ff
Alex Butler <lxbtlr@pm.me>
2026-08-12 11:36:17 -0500

    try disabling huge pages

--- include/common/runtime/Memory.hpp
+++ include/common/runtime/Memory.hpp
@@ -7,7 +7,7 @@ inline void* malloc_huge(size_t size) {
    void* p = mmap(nullptr, size, PROT_READ | PROT_WRITE,
                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 #ifdef __linux__
-   madvise(p, size, MADV_HUGEPAGE);
+   madvise(p, size); //MADV_HUGEPAGE);
 #endif
    // memset(p, 0, size);
    return p;
