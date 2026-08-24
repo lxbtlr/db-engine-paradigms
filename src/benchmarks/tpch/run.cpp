8561c61a4bfeb9bf85f8c42aac01b4230f7afc39
Alex Butler <lxbtlr@pm.me>
2026-08-24 16:10:24 -0500

    Fix broken code path + comment out verify q1 output function

--- src/benchmarks/tpch/run.cpp
+++ src/benchmarks/tpch/run.cpp
@@ -63,6 +63,7 @@ using namespace runtime;
 
 static void escape(void* p) { asm volatile("" : : "g"(p) : "memory"); }
 
+/*
 static void dumpQ1Result(const char* label, runtime::Query* query) {
    if (!query || !query->result) return;
    auto& rel = *query->result;
@@ -95,6 +96,7 @@ static void dumpQ1Result(const char* label, runtime::Query* query) {
    }
    fprintf(stderr, "=== END ===\n\n");
 }
+*/
 
 size_t nrTuples(Database& db, std::vector<std::string> tables) {
    size_t sum = 0;
@@ -315,24 +317,11 @@ int main(int argc, char* argv[]) {
       e.timeAndProfile(label("q1 v ", nrThreads), nrTuples(tpch, {"lineitem"}),
                        [&]() {
                           if (clearCaches) clearOsCaches();
-#ifdef VW_SPLIT_HASHGROUP
-                          auto result =
-                              q1_vectorwise_split(tpch, nrThreads, vectorSize);
-#else
                           auto result =
                               q1_vectorwise(tpch, nrThreads, vectorSize);
-#endif
                           escape(&result);
                        },
                        repetitions);
-      // Correctness dump
-#ifdef VW_SPLIT_HASHGROUP
-      auto vResult = q1_vectorwise_split(tpch, nrThreads, vectorSize);
-#else
-      auto vResult = q1_vectorwise(tpch, nrThreads, vectorSize);
-#endif
-// dumpQ1Result("vectorwise", vResult.get());
-   }
    if (q.count("3h"))
       e.timeAndProfile(label("q3 h ", nrThreads),
                        nrTuples(tpch, {"customer", "orders", "lineitem"}),
